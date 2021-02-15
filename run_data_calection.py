8b4cf89a5b55e022f94e83cda65e74e337016545
SerValera <43247005+SerValera@users.noreply.github.com>
2021-02-15 19:37:48 +0300

    Add files via upload

--- run_data_calection.py
+++ run_data_calection.py
@@ -1,25 +1,25 @@
 import csv
 
-#---ros init---
+# ---ros init---
 from geometry_msgs.msg import PoseStamped
 import rospy
+
 rospy.init_node('gesture', anonymous=True)
 pub1 = rospy.Publisher('/hand_position', PoseStamped, queue_size=10)
-#pub2 = rospy.Publisher('/gesture_number', PoseStamped, queue_size=10)
+# pub2 = rospy.Publisher('/gesture_number', PoseStamped, queue_size=10)
 
 data_out_position = PoseStamped()
 data_out_gesture = PoseStamped()
-rate = rospy.Rate(10) # 10hz
-#--------------
+rate = rospy.Rate(10)  # 10hz
+# --------------
 
 
-#variebles for get position
+# variebles for get position
 img_center_x = 250
 img_center_y = 250
 x_f = 0
 y_f = 0
 a_f = 0
-
 
 import cv2
 from src.hand_tracker import HandTracker
@@ -27,15 +27,15 @@ import math
 import numpy as np
 
 import warnings
-warnings.filterwarnings('ignore',category=FutureWarning)
 
+warnings.filterwarnings('ignore', category=FutureWarning)
 
 WINDOW = "Hand Tracking"
 PALM_MODEL_PATH = "../../catkin_ws/src/swarm_drones/scripts/models/palm_detection_without_custom_op.tflite"
 LANDMARK_MODEL_PATH = "../../catkin_ws/src/swarm_drones/scripts/models/hand_landmark.tflite"
 
-#PALM_MODEL_PATH = "models/pose_detection.tflite"
-#LANDMARK_MODEL_PATH = "models/pose_landmark_upper_body.tflite"
+# PALM_MODEL_PATH = "models/pose_detection.tflite"
+# LANDMARK_MODEL_PATH = "models/pose_landmark_upper_body.tflite"
 
 
 ANCHORS_PATH = "../../catkin_ws/src/swarm_drones/scripts/models/anchors.csv"
@@ -81,10 +81,11 @@ detector = HandTracker(
     box_enlarge=1.3
 )
 
-def get_size_pulm (pulm_key_point):
-    #calculation size of pulm
+
+def get_size_pulm(pulm_key_point):
+    # calculation size of pulm
     k_pulm = len(pulm_key_point)
-    distance_pulm = np.zeros(k_pulm-1)
+    distance_pulm = np.zeros(k_pulm - 1)
     size_pulm = 0
     for i in range(k_pulm - 1):
         x1 = points[i][0]
@@ -93,35 +94,34 @@ def get_size_pulm (pulm_key_point):
         y2 = points[i + 1][1]
         distance_pulm[i] = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
         size_pulm += distance_pulm[i]
-    size_pulm = size_pulm/k_pulm
-    #print('size_pulm: ', size_pulm)
+    size_pulm = size_pulm / k_pulm
+    # print('size_pulm: ', size_pulm)
     return size_pulm
 
+
 def get_sum_length(points):
-    #calculate length of finger
+    # calculate length of finger
     k = len(points)
-    distance = np.zeros(k-1)
+    distance = np.zeros(k - 1)
     sum = 0
-    for i in range(k-1):
+    for i in range(k - 1):
         x1 = points[i][0]
         y1 = points[i][1]
-        x2 = points[i+1][0]
-        y2 = points[i+1][1]
-        distance[i] = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        x2 = points[i + 1][0]
+        y2 = points[i + 1][1]
+        distance[i] = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
         sum += distance[i]
     sum = sum
     return sum
 
+
 i = 0
+
+gestures = ('one', 'two', 'three', 'four', 'five', 'ok', 'rock', 'thumbs_up', 'thumbs_down', 'close')
+
 while hasFrame:
-    print(i)
-    i+=1
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     points, _ = detector(image)
-
-    with open('database/gesture_two.csv', 'a') as f:
-        thewriter = csv.writer(f)
-        thewriter.writerow(points)
 
     if points is not None:
         for point in points:
@@ -132,7 +132,14 @@ while hasFrame:
             x1, y1 = points[connection[1]]
             cv2.line(frame, (int(x0), int(y0)), (int(x1), int(y1)), CONNECTION_COLOR, THICKNESS)
 
+        # print(i)
+        # i += 1
+        # with open('database_Alexeii/gesture_' + gestures[9] + '.csv', 'a') as f:
+        #     thewriter = csv.writer(f)
+        #     thewriter.writerow(points)
+
     color = (0, 0, 255)
+    frame = cv2.flip(frame, 1)
     cv2.imshow(WINDOW, frame)
     hasFrame, frame = capture.read()
     key = cv2.waitKey(1)
